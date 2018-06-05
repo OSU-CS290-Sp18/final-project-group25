9835628f9916b36475638ab5c4b41e830274e898
Nihar Doshi <doshin@flip2.engr.oregonstate.edu>
2018-06-05 15:30:49 -0700

    adding client js to public file

---
+++ public/index.js
@@ -0,0 +1,125 @@
+
+
+var buttonID  = document.getElementById('create-review-button');
+
+function handleToggleModal (event){
+	console.log("The button was clicked");
+	var modal = document.getElementById('create-review-modal');
+	modal.classList.toggle('hidden');
+  var modalBack = document.getElementById('modal-backdrop');
+  modalBack.classList.toggle('hidden');
+  var modalTwitText = modal.querySelector('.modal-body .review-input-element #review-text-input');
+  var modalTwitAuthor = modal.querySelector('.modal-body .review-input-element #review-attribution-input');
+  modalTwitText.value = '';
+  modalTwitAuthor.value = '';
+};
+
+buttonID.addEventListener('click', handleToggleModal);
+var xOutModal = document.getElementsByClassName('modal-close-button');
+var cancelModal = document.getElementsByClassName('modal-cancel-button');
+xOutModal[0].addEventListener('click', handleToggleModal);
+cancelModal[0].addEventListener('click', handleToggleModal);
+
+
+
+var searchBar = document.getElementById('nav-search-input');
+
+
+function handleSearchInput(event){
+	var reviews = document.querySelectorAll('.review');
+	var container = document.getElementsByClassName('review-container');
+	var input = document.getElementById('nav-search-input').value;
+	var twitsText;//= document.getElementsByClassName('twit-text');
+	var twitContent;
+	for(i = 0; i < twits.length; i++){
+  	twitsText = twits[i].getElementsByClassName('review-text');
+  	twitContent = twitsText[0].textContent;
+  	if(!twitContent.includes(input)){
+  	  // container[0].removeChild(twits[i]);
+  	  // twits[i].parentNode.removeChild(twits[i]);
+  	  twitsText[i].parentNode.parentNode.setAttribute('style', 'display:none;');
+  	}
+  	else{
+  	  twitsText[i].parentNode.parentNode.setAttribute('style', 'display:flex;');
+  	}
+	}
+}
+
+searchBar.addEventListener('input', handleSearchInput);
+
+var searchBarButton = document.getElementById('nav-search-button');
+searchBarButton.addEventListener('click', handleSearchInput);
+
+
+
+var createBtn = document.getElementsByClassName('modal-accept-button');
+
+function handleCreateReview(event){
+  var modal = document.getElementById('create-review-modal');
+  var modalReviewText = modal.querySelector('.modal-body .review-input-element #review-text-input');
+  var modalReviewAuthor = modal.querySelector('.modal-body .review-input-element #review-attribution-input');
+  if(modalReviewText.value == ''){
+    alert('You cannot create an empty review');
+    return;
+  }
+  if(modalReviewAuthor.value == ''){
+    alert('Every review must have an author');
+    return;
+  }
+  var newReview = document.createElement('article');
+  newReview.setAttribute('class', 'review');
+
+  console.log(modalReviewText.value);
+  console.log(modalReviewAuthor.value);
+  // var iconDiv = document.createElement('div');
+  // iconDiv.setAttribute('class', 'review-icon');
+  // var icon = document.createElement('i');
+  // icon.setAttribute('class', 'fas fa-bullhorn');
+  // iconDiv.appendChild(icon);
+
+
+  var textDiv = document.createElement('div');
+  textDiv.setAttribute('class', 'review-content');
+
+  var text = document.createElement('p');
+  text.setAttribute('class', 'review-text');
+  text.textContent = modalReviewText.value;
+  textDiv.appendChild(text);
+
+  var author = document.createElement('p');
+  author.setAttribute('class', 'review-author');
+  // var authorRef = document.createElement('a');
+  // authorRef.setAttribute('href', '#');
+  author.textContent = modalReviewAuthor.value;
+  // author.appendChild(authorRef);
+
+  textDiv.appendChild(text);
+  textDiv.appendChild(author);
+
+  // newTwit.appendChild(iconDiv);
+  newReview.appendChild(textDiv);
+
+  var container = document.querySelector('.review-container');
+
+  container.appendChild(newReview);
+
+
+  // var twitContainer = document.querySelector('main.twit-container');
+  // // twitContainer.appendChild(twitElem);
+
+  // var twitTemplate = Handlebars.templates.twit;
+  // var newTwitHTML = twitTemplate({
+  //   text: twitText,
+  //   author: twitAuthor
+  // });
+
+  // twitContainer.insertAdjacentHTML('beforeend', newTwitHTML);
+
+
+
+  handleToggleModal();
+}
+
+createBtn[0].addEventListener('click', handleCreateReview);
+
+
