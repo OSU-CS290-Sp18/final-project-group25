8e354a2e10208d38eeb831985d94c316f803b793
Harry <millharr@oregonstate.edu>
2018-06-10 21:20:05 -0700

    server changes for mongo

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || '27017';
+var mongoUsername = process.env.MONGO_USERNAME;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoURL = "mongodb://" + mongoUsername + ":" + mongoPassword +
+  "@" + mongoHost + ":" + mongoPort +  "/" + mongoDBName;
+
+var mongoDB = null;
 
 var itemData = require('./itemData');
 
@@ -9,6 +22,8 @@ var port = process.env.PORT || 3005;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
@@ -19,22 +34,25 @@ app.get('/', function(req, res){
 });
 
 app.get('/featured/:item', function (req, res, next) {
-  var item = Number(req.params.item);
-                            //insert number of items
-  if(true){
-    var firstItem = itemData["watermelon"];
-    var reviews = firstItem["reviews"];
-    var photos = firstItem["photos"];
-    var name = firstItem["name"];
-    res.status(200).render('itemPage', {
-      name: name,
-      photos: photos,
-      reviews: reviews
-    });
-  }
-  else{
-    next();
-  }
+  var item = req.params.person.toLowerCase();
+  var itemCollection = mongoDB.collection('items');
+  itemCollection.find({ itemId: item }).toArray(function (err, itemDocs){
+    if(err){
+      res.status(500).send("Error fetching item from DB.");
+    } else if(itemDocs.length > 0){
+      var firstItem = itemData["watermelon"];
+      var reviews = firstItem["reviews"];
+      var photos = firstItem["photos"];
+      var name = firstItem["name"];
+      res.status(200).render('itemPage', {
+        name: name,
+        photos: photos,
+        reviews: reviews
+      });
+    } else{
+      next();
+    }
+  });
 });
 
 app.get('*', function (req, res){
