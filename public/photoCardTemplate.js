ff3d469da74205d7b76211ef3f0365031022f669
Nihar Doshi <doshin@flip2.engr.oregonstate.edu>
2018-06-10 23:17:37 -0700

    bug fixes

--- public/photoCardTemplate.js
+++ public/photoCardTemplate.js
@@ -5,8 +5,10 @@ templates['photoCard'] = template({"compiler":[7,">= 4.0.0"],"main":function(con
 
   return "<section class=\"photo-item\">\r\n	<div class=\"image-container\">\r\n		<img class=\"product-image\" src="
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
-    + ">\r\n	</div>\r\n	<div class=\"item-name\">\r\n		"
+    + ">\r\n	</div>\r\n	<div class=\"item-name\">\r\n		<a href = \"/people/"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "\r\n	</div>\r\n</section>";
+    + "\">"
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + "</a>\r\n	</div>\r\n</section>";
 },"useData":true});
 })();
